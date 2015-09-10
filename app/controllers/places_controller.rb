8464d8bffafb92da7b74160c957698cf973e2dff
Nicola Guidi <nicola.guidi@me.com>
2015-09-10 14:01:49 +0000

    Allows a user to Edit a place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,6 +18,10 @@ before_action :authenticate_user!, :only => [:new, :create]
     @place = Place.find(params[:id])
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
   private
 
     def place_params
