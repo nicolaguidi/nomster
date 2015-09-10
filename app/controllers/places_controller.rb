75e1a5964735b9be84f9a0383be50420e2b4a77c
Nicola Guidi <nicola.guidi@me.com>
2015-09-10 13:29:26 +0000

    Create a page to show more details for a single place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,6 +14,10 @@ before_action :authenticate_user!, :only => [:new, :create]
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
   private
 
     def place_params
