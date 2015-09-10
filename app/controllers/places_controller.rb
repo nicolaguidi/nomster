086e2388fa6bdca62715e6e04945084353c1dce7
Nicola Guidi <nicola.guidi@me.com>
2015-09-10 14:13:07 +0000

    Updates places with new data

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,12 @@ before_action :authenticate_user!, :only => [:new, :create]
     @place = Place.find(params[:id])
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
   private
 
     def place_params
