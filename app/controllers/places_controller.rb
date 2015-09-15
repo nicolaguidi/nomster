35577f51df17b689a103863edd7ec8a176b922b4
Nicola Guidi <nicola.guidi@me.com>
2015-09-15 14:21:39 +0000

    Add a form to the individual place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,6 +20,7 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :des
 
   def show
     @place = Place.find(params[:id])
+    @comment = Comment.new
   end
 
   def edit
