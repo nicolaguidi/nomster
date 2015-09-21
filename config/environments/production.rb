2a5236f2bd8bcb80443a9dea86b24612eb1f1dd2
Nicola Guidi <nicola.guidi@me.com>
2015-09-21 13:56:44 +0000

    Configures Mailgun for notifications in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Nomster::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Mailgun setup:
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   # Devise setup (1)
   config.action_mailer.default_url_options = { host: 'nomster-nicola-guidi.herokuapp.com'}
 
