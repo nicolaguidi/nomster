1711eee349a8fa182a781b7785663b3cd9bdfcc5
Nicola Guidi <nicola.guidi@me.com>
2015-09-26 00:52:08 +0000

    Tests comments' creation

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,4 +4,19 @@ class CommentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'comment_create' do
+    user = FactoryGirl.create(:user)
+    sign_in user
+
+    place = FactoryGirl.create(:place)
+
+    assert_difference 'Comment.count' do
+      post :create, :place_id => place.id, :comment => {
+        :message => 'Okey Dokey',
+        :rating => '1_star'
+        }
+    end
+    assert_redirected_to place_path(place)
+    assert_equal 1, user.comments.count
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,11 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'users dashboard' do
+    user = FactoryGirl.create(:user)
+    sign_in user
+
+    get :show, :id => user.id
+    assert_response :success
+  end
 end
